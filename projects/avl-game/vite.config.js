a453bf4f5841efae2dec55d0d3f6ebad5410045d
Haris Ahmad <harisahmad@hotmail.ca>
2026-02-24 14:00:52 -0500

    fixing

--- projects/avl-game/vite.config.js
+++ projects/avl-game/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/avl-game/',  // ← Add this line
   plugins: [react()],
 })
