60ebb7b63e2c9e4ea8df34b142d88decf922762b
Haris Ahmad <harisahmad@hotmail.ca>
2026-02-28 21:17:27 -0500

    moved files out of dist, deleted docs

--- projects/avl-game/vite.config.js
+++ projects/avl-game/vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // 1. Use a relative base path instead of a hardcoded absolute path
-  base: '/avl-game/docs/',
   plugins: [react()],
-  
-  // 2. Configure the build process for GitHub pages
-  build: {
-    outDir: 'docs',       // Automatically puts the final files in the 'docs' folder
-    emptyOutDir: true,    // Clears out the old files in 'docs' before putting in the new ones
-  }
+  base: '/projects/avl-game/'
 })
